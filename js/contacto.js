0cb8bead5e5819192ac2331b2d94f550f3113d3f
agusvalori <61027212+agusvalori@users.noreply.github.com>
2022-05-19 19:15:40 -0300

    editando contacto js

--- js/contacto.js
+++ js/contacto.js
@@ -1,14 +1,13 @@
-require("firebase/firestore");
+console.log("Iniciando Contacto")
 var values
 
-const HandleInputChange = (event) => {
+const HandleInputChange = (event) => {  
     const { name, value } = event.target;
     values = { ...values, [name]: value };
   };
 
 const HandleOnSubmit = ()=>{
     event.preventDefault();
-
     console.log(values)
 
     document.getElementById("formContacto").reset()
